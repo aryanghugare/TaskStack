6f11193d55139a44b8a4bcb779b977ce29eef338
Aryan <aryanghugare75@gmail.com>
2025-11-04 09:53:44 +0530

    Validation setup and register feature  Validation

---
+++ src/middlewares/validator.middleware.js
@@ -0,0 +1,26 @@
+// What it does: This middleware checks for validation errors in the request using express-validator.
+// The files are added here , here this file will extract the errors and will process them accordingly. 
+// This file is reusable across different routes where validation is needed and also many other projects 
+
+import { validationResult } from "express-validator";
+import { ApiError, AppError } from "../utils/api-error.js"
+
+export const validate = (req, res, next) => {
+const errors = validationResult(req);
+if(errors.isEmpty()){
+return next();
+}
+const extractedErrors = [];
+errors.array().map(err => extractedErrors.push(
+{
+[err.path]: err.msg //You Could also push the whole err object if you want more details
+}
+));
+throw new ApiError(400, "Validation Error", extractedErrors);
+
+}
+
+
+
+
+
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import {registerUser} from "../controllers/auth.controllers.js"
+import { userRegisterValidator } from "../validators/index.js";
+import { validate } from "../middlewares/validator.middleware.js";
+
 const router = Router();
 
-router.route("/register").post(registerUser);
-
+router.route("/register").post(userRegisterValidator() ,validate,registerUser); // Here the thing is we are adding the validator middleware to the route before the controller function
+// validate is the middleware that will check for validation errors
+// userRegisterValidator is the function that returns an array of validation rules
 export default router;
